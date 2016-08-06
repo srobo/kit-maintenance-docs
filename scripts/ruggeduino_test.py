00606b3bdf2a96e8164cf6021ede4712ed76fdb3
Richard Barlow <richard@richardbarlow.co.uk>
2016-08-06 16:19:47 +0100

    Add Ruggeduino test procedure and script

---
+++ scripts/ruggeduino_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import subprocess
+import time
+
+test_fw = "firmware/ruggeduino_test-1.hex"
+fw = "firmware/ruggeduino-1.hex"
+
+def flash_ruggeduino(fw_hex):
+    subprocess.check_call(["avrdude", "-p", "atmega328p", "-c", "arduino",
+                           "-P" ,"/dev/ttyACM0",  "-D", "-U",
+                           "flash:w:{0}:i".format(fw_hex)])
+
+print "Flashing test firmware:"
+flash_ruggeduino(test_fw)
+print "Look at the LED. Slow flash = PASS; Fast flash = FAIL;"
+time.sleep(5)
+print "Flashing SR firmware:"
+flash_ruggeduino(fw)
---
+++ src/ruggeduino.md
@@ -0,0 +1,15 @@
+# Ruggeduino Maintenance[^1]
+
+## Equipment
+
+ * Laptop
+ * Full size USB cable
+ * Ruggeduino test shield
+
+## Procedure
+
+*Execution time*: 30 seconds per board.
+
+ 1. Plug the test shield into the Ruggeduino.
+ 1. Plug the Ruggeduino into the laptop.
+ 1. Run `./scripts/ruggeduino_test.py` and follow the instructions.
